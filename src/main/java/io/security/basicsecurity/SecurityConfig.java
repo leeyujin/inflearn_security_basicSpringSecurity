c06284e9174b36dde27c2b28b0b6c83e74acfb5c
yujinlee <now8567@hanmail.net>
2021-11-06 23:16:59 +0900

    필터 초기화와 다중 보안 설정

--- src/main/java/io/security/basicsecurity/SecurityConfig.java
+++ src/main/java/io/security/basicsecurity/SecurityConfig.java
@@ -1,64 +1,40 @@
 package io.security.basicsecurity;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
-import org.springframework.security.web.savedrequest.SavedRequest;
-
-import javax.servlet.http.HttpSession;
 
 @Configuration
 @EnableWebSecurity
+// order를 1로 주고 아래 config order를 0으로 주면, 인증필요없음
+@Order(0)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Autowired
-    UserDetailsService userDetailsService;
-
     @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().withUser("user").password("{noop}1111").roles("USER");
-        auth.inMemoryAuthentication().withUser("sys").password("{noop}1111").roles("SYS","USER");
-        auth.inMemoryAuthentication().withUser("admin").password("{noop}1111").roles("ADMIN","SYS","USER");
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .antMatcher("/admin/**")
+                .authorizeRequests()
+                .anyRequest().authenticated()
+        .and()
+                .httpBasic()
+        ;
     }
+}
+
+@Configuration
+@Order(1)
+class SecurityConfig2 extends WebSecurityConfigurerAdapter{
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/login").permitAll()
-                .antMatchers("/user").hasRole("USER")
-                .antMatchers("/admin/pay").hasRole("ADMIN")
-                .antMatchers("/admin/**").access("hasRole('SYS') or hasRole('ADMIN')")
-                .anyRequest().authenticated()
-        ;
-
-        http
+                .anyRequest().permitAll()
+        .and()
                 .formLogin()
-//                .defaultSuccessUrl("/login", true)
-                .successHandler(
-                        (httpServletRequest, httpServletResponse, authentication) -> {
-                            HttpSessionRequestCache httpSessionRequestCache = new HttpSessionRequestCache();
-                            SavedRequest request = httpSessionRequestCache.getRequest(httpServletRequest, httpServletResponse);
-                            String redirectUrl = request.getRedirectUrl();
-                            httpServletResponse.sendRedirect(redirectUrl);
-                        })
-        ;
-
-        http
-                .exceptionHandling()
-              /*  .authenticationEntryPoint(
-                        (httpServletRequest, httpServletResponse, e) -> httpServletResponse.sendRedirect("/login")
-                )*/
-                .accessDeniedHandler(
-                        (httpServletRequest, httpServletResponse, e) -> httpServletResponse.sendRedirect("/denied")
-                )
         ;
     }
-
 }
