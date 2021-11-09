cfb90ea6e1747a97367f39d55779a4b5f6372926
yujinlee <now8567@hanmail.net>
2021-11-09 20:38:58 +0900

    인증 저장소 - SecurityContextHolder, SecurityContext

--- src/main/java/io/security/basicsecurity/SecurityConfig.java
+++ src/main/java/io/security/basicsecurity/SecurityConfig.java
@@ -5,36 +5,23 @@ import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 @Configuration
 @EnableWebSecurity
-// order를 1로 주고 아래 config order를 0으로 주면, 인증필요없음
-@Order(0)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .antMatcher("/admin/**")
                 .authorizeRequests()
                 .anyRequest().authenticated()
-        .and()
-                .httpBasic()
         ;
-    }
-}
 
-@Configuration
-@Order(1)
-class SecurityConfig2 extends WebSecurityConfigurerAdapter{
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
         http
-                .authorizeRequests()
-                .anyRequest().permitAll()
-        .and()
                 .formLogin()
         ;
+
+        SecurityContextHolder.setStrategyName(SecurityContextHolder.MODE_INHERITABLETHREADLOCAL);
     }
 }
--- src/main/java/io/security/basicsecurity/SecurityController.java
+++ src/main/java/io/security/basicsecurity/SecurityController.java
@@ -1,43 +1,36 @@
 package io.security.basicsecurity;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpSession;
 
 @RestController
 public class SecurityController {
 
     @GetMapping("/")
-    public String index(){
+    public String index(HttpSession session){
+        // session에서 꺼내온 인증객체와,ContextHolder에서 꺼내온 방식의 인증객체는 동일함
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        SecurityContext context = (SecurityContext) session.getAttribute(HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY);
+        Authentication authentication1 = context.getAuthentication();
         return "home";
     }
 
-    @GetMapping("/loginPage")
-    public String loginPage(){
-        return "loginPage";
-    }
-    @GetMapping("/user")
-    public String home(){
-        return "user";
+
+    // SecurityContext 객체저장방식인 MODE_THREADLOCAL(default) 상태에서는 각 쓰레드별 ThreadLocal을 별도로 가지기 때문에 Main,자식 Thread간 인증정보는 다름
+    // 인증 Filter가 메인 쓰레드로컬에 인증객체를 담음 -> 자식 쓰레드 인증 값과 다름
+    @GetMapping("/thread")
+    public String thread(){
+        new Thread(() -> {
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        }).start();
+        return "thread";
     }
 
-    @GetMapping("/admin/pay")
-    public String adminPay(){
-        return "adminPay";
-    }
-
-    @GetMapping("/admin/**")
-    public String admin(){
-        return "admin";
-    }
-
-    @GetMapping("/denided")
-    public String denied(){
-        return "access denied";
-    }
-
-    @GetMapping("/login")
-    public String login(){
-        return "login";
-    }
 
 }
