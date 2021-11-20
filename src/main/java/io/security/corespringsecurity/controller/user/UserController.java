3361938f6616ff81eec23593a7f9c2e88c538490
yujinlee <now8567@hanmail.net>
2021-11-20 10:36:52 +0900

    inmemory -> DB연동을 이용한 로그인방식으로 수정

--- src/main/java/io/security/corespringsecurity/controller/user/UserController.java
+++ src/main/java/io/security/corespringsecurity/controller/user/UserController.java
@@ -1,6 +1,6 @@
 package io.security.corespringsecurity.controller.user;
-import io.security.corespringsecurity.domain.Account;
-import io.security.corespringsecurity.domain.AccountDto;
+import io.security.corespringsecurity.domain.entity.Account;
+import io.security.corespringsecurity.domain.dto.AccountDto;
 import io.security.corespringsecurity.service.UserService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,8 +8,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-
-import java.security.Principal;
 
 @Controller
 public class UserController {
